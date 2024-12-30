26a26e21ac5d9c252f68acaa1a7cc0f020328977
0EFB6 <changws12345@gmail.com>
2024-12-30 09:12:59 +0000

    Update Poker.sol
    Changes to be committed:
    	modified:   contracts/Poker.sol
    	modified:   next-client/bun.lockb
    	new file:   next-client/src/app/sc/page.tsx

--- contracts/Poker.sol
+++ contracts/Poker.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 contract Poker {
     uint256 public constant MIN_ENTRY_FEE = 1 ether;
+    address constant public cadenceArch = 0x0000000000000000000000010000000000000001;
 
     struct Game {
         address player1;
@@ -50,6 +51,13 @@ contract Poker {
         game.player2 = address(0);
         payable(msg.sender).transfer(game.pool);
     }
+
+    function vrf() public view returns (uint64) {
+        (bool ok, bytes memory data) = cadenceArch.staticcall(abi.encodeWithSignature("revertibleRandom()"));
+        require(ok, "Failed to fetch a random number through Cadence Arch");
+        uint64 randomNumber = abi.decode(data, (uint64));
+        return (randomNumber % (52)) + 1;
+	}
 
     // Fallback function to prevent accidental $FLOW transfers
     receive() external payable {
